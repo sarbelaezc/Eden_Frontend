c127f522a8f6b722281456cb174d5f591dd1f4e1
Santiago Arbeláez Cataño <santiarbeca@hotmail.com>
2026-01-09 00:16:10 -0500

    feat: agregar configuración de cliente HTTP y manejador de interceptores en la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './core/services/auth.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 };
