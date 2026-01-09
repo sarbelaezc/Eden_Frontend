8f688ee32ce942202ceccd911d1f4d6cb87ebfc3
Santiago Arbeláez Cataño <santiarbeca@hotmail.com>
2026-01-09 00:16:26 -0500

    feat: agregar rutas para el componente de inicio de sesión y proteger las rutas de dashboard y settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
+  },
   {
     path: '',
     redirectTo: 'dashboard',
@@ -8,10 +15,12 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard',
+    canActivate: [authGuard],
     loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
   },
   {
     path: 'settings',
+    canActivate: [authGuard],
     loadChildren: () => import('./features/settings/settings.routes').then(m => m.SETTINGS_ROUTES)
   }
 ];
